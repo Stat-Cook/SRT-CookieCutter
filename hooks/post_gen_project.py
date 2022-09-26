31ef4a97e8795556c0de35f1951c135225498ffb
Rob <rzc1@staff.staffs.ac.uk>
2022-09-26 11:58:15 +0100

    Add package checks for ease of use

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,17 +2,33 @@ import os
 import sys
 from shutil import rmtree
 
-import analysis_cli
+try:
+    import analysis_cli
+except ModuleNotFoundError:
+    repo_address = "https://github.com/Stat-Cook/Analysis-CookieCutter"
+    raise(ModuleNotFoundError(
+        f"Module 'analysis_cli' not found \n run 'pip install git+{repo_address}' to install. ")
+    )
 
+try:
+    import docx
+except ModuleNotFoundError:
+    raise(ModuleNotFoundError(
+        f"Module 'docx' not found \n run 'pip install python-docx' to install. ")
+    )
+
+# Set up project to read
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 sys.path.insert(0, PROJECT_DIRECTORY)  # add the working directory to the path
 from hook_modules import DirMaker, DocReplacer, remove_files
 
+# Assign useful variables
 WP_n = {{cookiecutter.WP}}
 project_name = '{{cookiecutter.project_name}}'
 
 wp_f = lambda i: f"5.{i+2} WP{i+1}"
 
+# Make data folder tree
 data_root = DirMaker("5. Anonymised Data (No PID)", "5")
 data_root.make_child("Resources")
 
@@ -24,12 +40,10 @@ for i in range(WP_n):
     analysis_root = os.path.join(PROJECT_DIRECTORY, wp_root.parent)
     analysis_cli.cutter(i+1, 1, analysis_root)
 
-
-print(PROJECT_DIRECTORY)
-
+# Replace occurrences of key words in word docs
 dr = DocReplacer(".")
 dr()
 
+# Remove vestigial files
 rmtree("hook_modules")
-
 remove_files(".")
